ba14157ef71c602fb062d166f8d9c568aaee17d9
Johan <johan.kruse@gmail.com>
2019-05-23 12:09:27 +0200

    Uploaded package, changed package name to scoped

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, Theme } from '@material-ui/core'
+import { ThemeProviderProps } from '@material-ui/styles/ThemeProvider'
 
 // We only have 1 theme, KISS.
 import theme, { Button as StyledButton } from './themes/codaisseur'
 
 // This is the new theme provider.
-export const ThemeProvider = (props: any) => (
+export const ThemeProvider = (props: ThemeProviderProps<Theme>): JSX.Element => (
   <MuiThemeProvider {...props} theme={theme}>
     <CssBaseline />
     {props.children}
