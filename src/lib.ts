c6cb19904254845b5f59cc69bb1df4aea52cbc77
Johan <johan.kruse@gmail.com>
2019-06-13 10:53:40 +0200

    Updated deps, added SSR fn withRoot

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,8 @@
+import { createGenerateClassName } from '@material-ui/styles'
+import { StylesOptions } from '@material-ui/styles/StylesProvider'
+import { SheetsRegistry } from 'jss'
+import { GenerateClassNameOptions } from '@material-ui/styles/createGenerateClassName';
+
 /**
  * Convert Pixel to Point (spacing unit)
  *
@@ -9,3 +14,44 @@ export const px2pt = (px: number) => {
   return px / 8 + 'rem'
 }
 export const px2unit = px2pt
+
+/**
+ * Default settings of the withRoot, for SSR.
+ * https://material-ui.com/styles/api/#stylesprovider
+ *
+ * @param {StylesOptions} options Override default settings.
+ * @param {GenerateClassNameOptions} options Override default settings.
+ */
+
+export const createPageContext = (
+  options?: StylesOptions,
+  classNameOptions?: GenerateClassNameOptions
+): StylesOptions => ({
+  sheetsManager: new Map(),
+  sheetsRegistry: new SheetsRegistry(),
+  generateClassName: createGenerateClassName({ ...classNameOptions }),
+  ...options,
+})
+
+/**
+ * Handler for the Page Context, making sure the Context is only created when needed.
+ * https://material-ui.com/styles/api/#stylesprovider
+ */
+
+export const getPageContext = (
+  options?: StylesOptions,
+  classNameOptions?: GenerateClassNameOptions
+): StylesOptions => {
+  // Make sure to create a new context for every server-side request so that data
+  // isn't shared between connections (which would be bad).
+  if (!(process as any).browser) {
+    return createPageContext(options, classNameOptions)
+  }
+
+  // Reuse context on the client-side.
+  if (!(global as any).__INIT_MATERIAL_UI__) {
+    ;(global as any).__INIT_MATERIAL_UI__ = createPageContext(options, classNameOptions)
+  }
+
+  return (global as any).__INIT_MATERIAL_UI__
+}
