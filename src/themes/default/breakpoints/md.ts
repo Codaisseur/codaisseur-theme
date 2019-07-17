64510b62feaf129044ae16c48649de2f9106e908
irenedeni <irene@codaisseur.com>
2019-07-17 10:40:19 +0200

    added breakpoints files for typography

---
+++ src/themes/default/breakpoints/md.ts
@@ -0,0 +1,6 @@
+import { h1 as default_h1 } from '../typography'
+
+export const h1 = {
+  ...default_h1,
+  fontSize: '48px',
+}
---
+++ src/themes/default/breakpoints/xs.ts
@@ -0,0 +1,11 @@
+import { h1 as default_h1, h2 as default_h2 } from '../typography'
+
+export const h1 = {
+  ...default_h1,
+  fontSize: '48px',
+}
+
+export const h2 = {
+  ...default_h2,
+  fontSize: '34px',
+}
