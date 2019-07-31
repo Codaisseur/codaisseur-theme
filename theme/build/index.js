f2636ed80316fde042e590aaf9fcf053f2a0d997
irenedeni <irene@codaisseur.com>
2019-07-31 15:14:00 +0200

    removed errors

---
+++ theme/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var styles_1 = require("@material-ui/core/styles");
+exports.theme = {
+    typography: {
+        fontSize: 100
+    }
+};
+exports["default"] = styles_1.createMuiTheme(exports.theme);
---
+++ theme/src/index.ts
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+export const theme = {
+  typography: {
+    fontSize: 100,
+  },
+}
+export default createMuiTheme(theme)
